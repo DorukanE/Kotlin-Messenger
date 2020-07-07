dd72ce3ccf63837f9d3d4e683fc49ee6056f1709
DorukanE <dorukaneskiceri@gmail.com>
2020-07-07 16:07:53 +0300

    some activites created.

---
+++ app/src/main/java/com/dorukaneskiceri/kotlinmessenger/LoginActivity.kt
@@ -0,0 +1,17 @@
+package com.dorukaneskiceri.kotlinmessenger
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+}
--- app/src/main/java/com/dorukaneskiceri/kotlinmessenger/MainActivity.kt
+++ app/src/main/java/com/dorukaneskiceri/kotlinmessenger/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.dorukaneskiceri.kotlinmessenger
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+
+        registerButton.setOnClickListener {
+            val email = emailTextRegister.text.toString()
+            val password = passwordTextRegister.text.toString()
+            println("Email: $email")
+            println("Password: $password")
+        }
+
+        accountText.setOnClickListener {
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

4f1f64f5faec312ed35808db086c57c54ed3a6a9
DorukanE <dorukaneskiceri@gmail.com>
2020-07-07 18:38:58 +0300

    firebase login service completed.

--- app/src/main/java/com/dorukaneskiceri/kotlinmessenger/LoginActivity.kt
+++ app/src/main/java/com/dorukaneskiceri/kotlinmessenger/LoginActivity.kt
@@ -1,17 +1,41 @@
 package com.dorukaneskiceri.kotlinmessenger
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
 
+        loginButton.setOnClickListener {
+            doLogin()
+        }
 
+        backToRegisterText.setOnClickListener{
+            finish()
+        }
     }
 
+    private fun doLogin(){
+        val loginAuth = FirebaseAuth.getInstance()
+        val email = emailLogin.text.toString()
+        val password = passwordLogin.text.toString()
+        if(email.isNotEmpty() && password.isNotEmpty()){
+            loginAuth.signInWithEmailAndPassword(email,password).addOnSuccessListener {
+                println("Sign In completed.")
+                Toast.makeText(this,"Sign in successful.",Toast.LENGTH_LONG).show()
+            }.addOnFailureListener {
+                Toast.makeText(this,"Username or Password is incorrect.",Toast.LENGTH_LONG).show()
+                println(it.localizedMessage.toString())
+            }
+        }else{
+            Toast.makeText(this,"Please fill the inputs correctly.",Toast.LENGTH_LONG).show()
+        }
+    }
 }

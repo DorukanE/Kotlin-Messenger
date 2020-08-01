63dc391d0b6c7168219029372f7c4765cc0fa5a3
DorukanE <dorukaneskiceri@gmail.com>
2020-08-01 14:13:13 +0300

    splash screen added.

---
+++ app/src/main/java/com/dorukaneskiceri/kotlinmessenger/registerlogin/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.dorukaneskiceri.kotlinmessenger.registerlogin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.dorukaneskiceri.kotlinmessenger.R
+import com.dorukaneskiceri.kotlinmessenger.messages.LatestMessagesActivity
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent(this,LatestMessagesActivity::class.java)
+            startActivity(intent)
+        },1300)
+    }
+}
